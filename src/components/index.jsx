6ee26b28e0cbe409f5dc03dc21c32fafcec74983
Havre Loic <dongmoabenahavreloic030@gmail.com>
2023-02-04 08:38:12 +0100

    first commit

---
+++ src/components/index.jsx
@@ -0,0 +1,5 @@
+export { default as Navbar } from './navbar/navbar.component';
+export { default as Feed } from './feed/feed.component';
+export { default as VideoDetail } from './video-detail/video-detail.component';
+export { default as ChannelDetail } from './channel-detail/channel-detail.component';
+export { default as SearchFeed } from './search-feed/search-feed.component';
